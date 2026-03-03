d0990da4113e888893fb15793b102dd8a5f914d2
gopinathdhara <gopi1.dhara@gmail.com>
2026-03-04 01:51:54 +0530

    navbar css

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function NavBar() {
   return (
     <>
-    <div className="flex gap-4">
+    <div className="flex gap-4 nav-bar-cls">
  <Link to="/">
         <img
           className="w-[70px]"
