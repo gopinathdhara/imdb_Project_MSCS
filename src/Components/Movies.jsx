d9deb49d9ee6093bed668c1d903c56c12f36cdf5
gopinathdhara <gopi1.dhara@gmail.com>
2026-03-07 14:27:15 +0530

    useref changes

--- src/Components/Movies.jsx
+++ src/Components/Movies.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import fetchMovies from "../Redux/fetchMovies";
 import { useNavigate } from "react-router-dom";
@@ -17,7 +17,6 @@ function Movies() {
   const TOKEN = import.meta.env.VITE_TMDB_TOKEN;
   //useDispatch hook
   const dispatch = useDispatch();
-
   const { movieSlc, loading, error } = useSelector((state) => state.movies);
   const pageNo = useSelector((state) => state.pagination.pageNo);
   const watchList = useSelector((state) => state.watch_list.watchlist);
@@ -41,6 +40,15 @@ function Movies() {
 
   // Render 2
   // → Effect 1 runs again (because pageNo changed)
+
+  // useRef is used to access DOM elements directly.
+  //  It allows us to call DOM methods like focus() without causing component re-render.
+  const inputRef = useRef(null);
+
+  // useref
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, []);
 
   useEffect(() => {
     if (!TOKEN) {
@@ -92,120 +100,146 @@ function Movies() {
   //#################################################################################
 
   // if (loading) return <div className="text-center mt-10">Loading...</div>;
-  if (loading)
-    return (
-      <div className="flex flex-col items-center justify-center mt-20">
-        <div className="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
-        <p className="mt-4 text-gray-600 font-semibold">Loading movies...</p>
-      </div>
-    );
+  // if (loading)
+  //   return (
+  //     <div className="flex flex-col items-center justify-center mt-20">
+  //       <div className="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+  //       <p className="mt-4 text-gray-600 font-semibold">Loading movies...</p>
+  //     </div>
+  //   );
   // if (error)
   //   return <div className="text-center mt-10">Error loading movies</div>;
 
-  if (error)
-    return (
-      <div className="flex flex-col items-center justify-center min-h-[60vh] text-center px-4">
-        <div className="bg-red-100 text-red-600 p-4 rounded-full mb-4">❌</div>
+  // if (error)
+  //   return (
+  //     <div className="flex flex-col items-center justify-center min-h-[60vh] text-center px-4">
+  //       <div className="bg-red-100 text-red-600 p-4 rounded-full mb-4">❌</div>
 
-        <h2 className="text-xl font-bold text-red-600">
-          Oops! Something went wrong.
-        </h2>
+  //       <h2 className="text-xl font-bold text-red-600">
+  //         Oops! Something went wrong.
+  //       </h2>
 
-        <p className="text-gray-600 mt-2">
-          We couldn't load the movies. Please try again.
-        </p>
+  //       <p className="text-gray-600 mt-2">
+  //         We couldn't load the movies. Please try again.
+  //       </p>
 
-        <button
-          onClick={() => window.location.reload()}
-          className="mt-5 bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-lg transition"
-        >
-          Retry
-        </button>
-      </div>
-    );
+  //       <button
+  //         onClick={() => window.location.reload()}
+  //         className="mt-5 bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-lg transition"
+  //       >
+  //         Retry
+  //       </button>
+  //     </div>
+  //   );
 
   return (
     <>
       <div className="text-2xl font-bold text-center m-5">
         <h1>Trending Movies</h1>
+        {/*useref */}
         <div className="flex justify-center my-4">
           <input
             type="text"
             placeholder="Search Movies..."
             value={searchText}
             onChange={(e) => setSearchText(e.target.value)}
+            ref={inputRef}
             className="border border-gray-400 rounded-md px-4 py-2 w-80 focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
         </div>
       </div>
-      <div className="flex justify-evenly text-center gap-8 flex-wrap">
-        {movieSlc?.map((item, index) => {
-          const imgPath = item.backdrop_path || item.poster_path;
-          const check_if_contain = doescontain(item);
-          const check_if_favourite_contain = doesContainFavourite(item);
-          return (
-            <div className="movie-card">
-              <div
-                key={item.id}
-                className="h-[40vh] w-[200px] rounded-xl flex flex-col bg-cover bg-center flex poster-wrap"
-                style={{
-                  backgroundImage: `url(https://image.tmdb.org/t/p/original/${imgPath})`,
-                  cursor: "pointer",
-                }}
-              >
-                {check_if_contain ? (
-                  <div
-                    className="m-4 flex watchlist-icon"
-                    onClick={() => removeFromWatchList(item)}
-                  >
-                    ❌
-                  </div>
-                ) : (
-                  <div
-                    className="m-4 flex watchlist-icon"
-                    onClick={() => addToWatchList(item)}
-                  >
-                    😍
-                  </div>
-                )}
+      {loading && (
+        <div className="flex flex-col items-center justify-center mt-10">
+          <div className="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+          <p className="mt-4 text-gray-600 font-semibold">Loading movies...</p>
+        </div>
+      )}
 
-                <div className="text-white text-xl p-2">{item.title}</div>
+      {error && (
+        <div className="flex flex-col items-center justify-center min-h-[30vh] text-center px-4">
+          <div className="bg-red-100 text-red-600 p-4 rounded-full mb-4">
+            ❌
+          </div>
+          <h2 className="text-xl font-bold text-red-600">
+            Oops! Something went wrong.
+          </h2>
+          <p className="text-gray-600 mt-2">
+            We couldn't load the movies. Please try again.
+          </p>
+        </div>
+      )}
+      {!loading && !error && (
+        <>
+          <div className="flex justify-evenly text-center gap-8 flex-wrap">
+            {movieSlc?.map((item, index) => {
+              const imgPath = item.backdrop_path || item.poster_path;
+              const check_if_contain = doescontain(item);
+              const check_if_favourite_contain = doesContainFavourite(item);
+              return (
+                <div className="movie-card" key={item.id}>
+                  <div
+                    key={item.id}
+                    className="h-[40vh] w-[200px] rounded-xl flex flex-col bg-cover bg-center flex poster-wrap"
+                    style={{
+                      backgroundImage: `url(https://image.tmdb.org/t/p/original/${imgPath})`,
+                      cursor: "pointer",
+                    }}
+                  >
+                    {check_if_contain ? (
+                      <div
+                        className="m-4 flex watchlist-icon"
+                        onClick={() => removeFromWatchList(item)}
+                      >
+                        ❌
+                      </div>
+                    ) : (
+                      <div
+                        className="m-4 flex watchlist-icon"
+                        onClick={() => addToWatchList(item)}
+                      >
+                        😍
+                      </div>
+                    )}
 
-                {/* Movie Details Button */}
-              </div>
-              <div style={{ display: "flex" }}>
-                {check_if_favourite_contain ? (
-                  <div
-                    className="m-4 flex favorite-icon"
-                    onClick={() => removeToFavouriteList(item)}
-                  >
-                    <i className="fa-solid fa-heart"></i>
+                    <div className="text-white text-xl p-2">{item.title}</div>
+
+                    {/* Movie Details Button */}
                   </div>
-                ) : (
-                  <div
-                    className="m-4 flex favorite-icon"
-                    onClick={() => addToFavouriteList(item)}
-                  >
-                    <i className="fa-regular fa-heart"></i>
+                  <div style={{ display: "flex" }}>
+                    {check_if_favourite_contain ? (
+                      <div
+                        className="m-4 flex favorite-icon"
+                        onClick={() => removeToFavouriteList(item)}
+                      >
+                        <i className="fa-solid fa-heart"></i>
+                      </div>
+                    ) : (
+                      <div
+                        className="m-4 flex favorite-icon"
+                        onClick={() => addToFavouriteList(item)}
+                      >
+                        <i className="fa-regular fa-heart"></i>
+                      </div>
+                    )}
+                    <Link to={`/movie-details/${item.id}`}>
+                      <button class="mybutton">View Details</button>
+                    </Link>
                   </div>
-                )}
-                <Link to={`/movie-details/${item.id}`}>
-                  <button class="mybutton">View Details</button>
-                </Link>
-              </div>
-            </div>
-          );
-        })}
-      </div>
-      <div className="bg-gray-400 p-4 h-[50px] mt-8 w-full flex justify-center gap-4">
-        <button onClick={handleprev}>
-          <i class="fa-solid fa-arrow-left"></i>
-        </button>
-        {pageNo}
-        <button onClick={handlenext} disabled={pageNo >= totalPages}>
-          <i class="fa-solid fa-arrow-right"></i>
-        </button>
-      </div>
+                </div>
+              );
+            })}
+          </div>
+          <div className="bg-gray-400 p-4 h-[50px] mt-8 w-full flex justify-center gap-4">
+            <button onClick={handleprev}>
+              <i class="fa-solid fa-arrow-left"></i>
+            </button>
+            {pageNo}
+            <button onClick={handlenext} disabled={pageNo >= totalPages}>
+              <i class="fa-solid fa-arrow-right"></i>
+            </button>
+          </div>
+        </>
+      )}
     </>
   );
 }
